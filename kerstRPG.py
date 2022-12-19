b95d0030560d964ea353c578a77f81a25bb82ec3
Kevinsimon046 <511027@vistacollege.nl>
2022-12-19 12:51:46 +0100

    added new code

--- kerstRPG.py
+++ kerstRPG.py
@@ -5,13 +5,15 @@ from art import *
 
 klaar_knop = input("This game contains a scary story, make sure you play this in a safe place... (press any button) ")
 klaar_knop = ()
-print("Before we begin, a special thanks to Kevin & Wadih for making this game")
+print("Before we begin, a special thanks to Kevin & Wadih for making this game.")
 print("Lets begin the story...")
 time.sleep(3)
 print("Prepare yourself😈...")
 time.sleep(3)
-print("You're a boy named Griffin who is 17 years old. Everyday you go to school by train, you got bullied many times at school and run to the woods nearby, but the bully is running after you. You look back but you hit a tree and fall unconscious.")
+print("CHAPTHER ONE: The House.")
 time.sleep(3)
+print("You're a boy named Griffin who is 17 years old. You go to school by train everyday, you got bullied many times at school and run to the woods nearby, but the bully is running after you. You look back but then you hit a tree and fall unconscious.")
+time.sleep(6)
 print("You finally wake up in the \x1B[3m" + "christmas" + "\x1B[0m woods next to an old house.")
 print("""\n        `'::.
     _________H ,%%&%,
@@ -22,7 +24,7 @@ print("""\n        `'::.
 ~~@._|@@_|||_@@|~||~~~~~~~~~~~~~""")
 
 
-keuze_huis = input("You decide to go check it... first the [INSIDE] or check the [OUTSIDE]\n ")
+keuze_huis = input("You decide to go check it... first the [INSIDE] or check the [OUTSIDE]?\n ")
 
 if keuze_huis == "inside":
     print("You discide to check the inside.\n ")
@@ -63,15 +65,16 @@ elif keuze_schuur == "stay":
     print("once you found out there was no one whispering but speaking to himself you found out that there was an old slave of Krampus... ")
     time.sleep(4)
     print("He was trapped inside of a cage ")
-    print("""            ================================
-             ||     ||<(.)>||<(.)>||     || 
-             ||    _||     ||     ||_    || 
-             ||   (__D     ||     C__)   || 
-             ||   (__D     ||     C__)   ||
-             ||   (__D     ||     C__)   ||
-             ||   (__D     ||     C__)   ||
-             ||     ||     ||     ||     ||
-             ================================""")
+    print("""\n
+================================
+||     ||<(.)>||<(.)>||     || 
+||    _||     ||     ||_    || 
+||   (__D     ||     C__)   || 
+||   (__D     ||     C__)   ||
+||   (__D     ||     C__)   ||
+||   (__D     ||     C__)   ||
+||     ||     ||     ||     ||
+================================""")
     
     time.sleep(5)
     print("you tried to talk to him but he was to afraid to talk to you... ")
@@ -80,7 +83,8 @@ elif keuze_schuur == "stay":
     time.sleep(5)
     print("he looked so hungry so you gave him something to eat that you have been stored in your pockets")
     time.sleep(4)
-    print("""    __________________,.............,    
+    print("""\n
+    __________________,.............,    
    /_/_/_/_/_/_/_/_/,-',  ,. -,-,--/|
   /_/_/_/_/_/_/_/,-' //  /-| / /--/ /
  /_/_/_/_/_/_/,-' `-''--'  `' '--/ /
@@ -93,11 +97,49 @@ elif keuze_schuur == "stay":
 
 keuze_bevrijden = random.randint(1, 10)
 
-if keuze_bevrijden < 3:
+if keuze_bevrijden <= 3:
     print("Krampus killed you!")
 
 else:
-    print("you freed the old slave")
+    print("you freed the old slave!")
     
+    
+schuur_bevrijding = print("you did free him so now he opened the door of the shed to the forest so you could hunt for Krampus and free all the children he kidnapped.")
+time.sleep(5)
+print("you walked through the scary dark lurid forest. it was so dark you couldn't even see anything")
+time.sleep(4)
+print("Suddenly you heard something rustle behind you...")
+keuze_forest = input("do you want to [CHECK] it or [IGNORE] it? ")
+if keuze_forest == "check":
+    print("you looked for a couple of minutes but you did not found something so you walked further.")
+    
+else:
+    print("you ignored the sound so you walked furhter.")
 
+time.sleep(4)
+print("you heard the sound again but this time it was way scarier.")
+time.sleep(4)
+print("the sound sounded like someone was dying of pain and someone else was crying...")
+time.sleep(4)
+print("you're brave enough to check the sound out. but then suddenly you found a map. ")
+time.sleep(4)
+print("a map leading to a huge house from Krampus where the kids have been trapped.")
+time.sleep(4)
+print("he was scared but brave enough to take a run to the house")
+time.sleep(4)
+print("you have arrived at the huge house")
+time.sleep(4)
+print("it was big, scary, dark, creepy and there were broken toys and teddy bears… also there was scuffed furniture in front of the house")
+time.sleep(6)
+print("you went in quickly. actually broken into")
+time.sleep(3)
+print("there he was... Krampus.")
+time.sleep(2)
+print("you hid on top of the roof luckily he didn't see you.")
+time.sleep(3)
+print("the hall in front of the room with children there where so many guards")
+time.sleep(4)
+print("you had the option to go [AGRESSIVE MODE] or [SILENT MODE]")
 
+keuze_aanval = input
+    
