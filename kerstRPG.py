ee9739801ac2eb0b883d4811efd9313f8b19b4c2
Wadih2006 <512373@vistacollege.nl>
2022-12-21 18:09:43 +0100

    einde verhaal afgemaakt

--- kerstRPG.py
+++ kerstRPG.py
@@ -242,8 +242,6 @@ else:
     print("He falls on the ground and you finish him with your last attack!!")
 
 
-
-# wadih gemaakt
 print("Finally Krampus is killed and all the fear is lost")
 time.sleep(2)
 keuze_zwaard = input("You are returning to the old monster, do you want to give him the sword back, [YES] or [NO]?")
@@ -260,11 +258,22 @@ if keuze_zwaard == "yes":
         time.sleep(3)
         print("\nThat is the end of the RPG, thanks to Kevin Simon & Wadih Ibrahim")
 else:
-    print("You didn't give him the sword back, instead, you bring it with you home and ask him the way back to the portal...")
+    print("You didn't give him the sword back, instead, you bring it with you home and want to put it in your room. You asked him the way back to the portal...")
     time.sleep(2)
-    print("You woke up at the door, the door was already open and you walk in.")
-    time.sleep(3)
+    print("You go home and your mother was waiting at the door and asks you where you have been.")
+    time.sleep(2)
+    keuze_leugen = input("[1]I stayed overnight with a friend, [2]I had to survival in the woods for school, [3]You say nothing and walks further...")
+    if keuze_leugen == "1":
+        print("Your parents believe you but are a little bit stressed")
+        time.sleep(3)
+    elif keuze_leugen == "2":
+        print("Your parents don't believe this and call your school to ask")
+        time.sleep(3)
+        print("To be continued...")
+        time.sleep(3)
+    elif keuze_leugen == "3":
+        print("Your mother calls the police to ask if they know anything about this")
+        time.sleep(3)
+        print("To be continued...")
+        time.sleep(3)
     print("\nThat is the end of the RPG, thanks to Kevin Simon & Wadih Ibrahim")
-
-
-    
--- test.py
+++ test.py
@@ -1,47 +1,9 @@
-import time
+import random
 
-print("Finally Krampus is killed and all the fear is lost")
-time.sleep(2)
-keuze_zwaard = input("You are returning to the old monster, do you want to give him the sword back, [YES] or [NO]? ")
-if keuze_zwaard == "yes":
-    print("The sword is back with his original owner, you ask him the way back to the portal...")
-    time.sleep(2)
+keuze_bevrijden = random.randint(1, 10)
 
-elif keuze_zwaard == "no":
-    print("You didn't give the sword back and asked the way back to the portal")
-    time.sleep(3)
-    print("Instead you put it later in your room so you will never forget")
-    time.sleep(2)
-    print("You went home and your mother opens the door and askes you where you were")
-
+if keuze_bevrijden <= 3:
+    print("Krampus killed you!")
+    exit()
 else:
-    print("You didn't give him the sword back, instead, you bring it with you home and ask him the way back to the portal...")
-    time.sleep(2)
-    print("You woke up at the door, the door was already open and you walk in.")
-    time.sleep(3)
-    print("\nThat is the end of the RPG, thanks to Kevin Simon & Wadih Ibrahim")
-
-    # keuze1 = "I stayed overnight with a friend"
-    # keuze2 = "I had to survive in the woods for school"
-    # keuze3= "you say nothing and keep walking"
-
-    # keuze_leugen = input("You have 3 options to choose of... \n[1]you can say you stayed with a friend, \n[2]survive in the woods for school or \n[3]you say nothing and keep walking")
-    # if keuze_leugen == "1":
-    #     print("your parents believe you but are a little stressed")
-    # elif keuze_leugen == "2":
-    #     print("")
-
-
-
-
-
-    keuze_bedanken = input("Do you want to thank him that he give you his sword? [YES] or [NO]...")
-    if keuze_bedanken == "yes":
-        print("You thanked him very much and you go back home. You woke up at the door, the door was already open and you walk in.")
-        time.sleep(3)
-        print("\nThat is the end of the RPG, thanks to Kevin Simon & Wadih Ibrahim")
-    elif keuze_bedanken == "no":
-        print("You didn't thank him and got back home. You woke up at the door, the door was already open and you walk in.")
-        time.sleep(3)
-        print("\nThat is the end of the RPG, thanks to Kevin Simon & Wadih Ibrahim")
-
+    print("You freed the old slave!")
